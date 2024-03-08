2e39b12cc5051f8e3ae96ed3ca7aba959f1e97b0
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-03-08 09:25:58 +0100

    chore: init backend

---
+++ backend/src/test/kotlin/org/mw/holgithub/HolGithubApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.mw.holgithub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HolGithubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
