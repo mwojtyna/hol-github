8171837fbe51f66f630a81ecbd7d54fee8303fdf
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-03-16 21:57:01 +0100

    feat: use dotenv, remove length constraints in request bodies

--- backend/src/main/kotlin/org/mw/holgithub/dto/ApiUserSignupPostRequest.kt
+++ backend/src/main/kotlin/org/mw/holgithub/dto/ApiUserSignupPostRequest.kt
@@ -1,14 +1,10 @@
 package org.mw.holgithub.dto
 
+import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Pattern
-import jakarta.validation.constraints.Size
 
 data class ApiUserSignupPostRequest(
-    @field:Size(
-        min = 1,
-        max = 255,
-        message = "Username must be between 1 and 255 characters"
-    ) val username: String,
+    @field:NotEmpty(message = "Username must not be empty") val username: String,
 
     @field:Pattern(
         regexp = """^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$""", message = "Invalid password"
